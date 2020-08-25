777c56e862013716686860920a949803252825ac
Jacob Robinson <jrobinsondev@gmail.com>
2020-08-25 12:11:46 -0400

    add first two test cases

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import { Server } from 'http';
 import url from 'url';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 import app from '../src/app';
 
@@ -39,73 +39,101 @@ describe('Feathers application tests', () => {
     it('Case 1: Stewie Griffin makes a deposit', async () => {
       try {
         const stewiesDeposit = await axios.post(getUrl('transactions'), {
-          headers: {
-            'Accept': 'text/html'
-          }
+          data: JSON.stringify({
+            // token: whatever we use for auth
+            accountFrom: 1234,
+            amount: 300,
+            currency: 'USD'
+          })
         });
-        // assert.equal(stewiesDeposit.finalBalance, 400)
+
+        assert.strictEqual(stewiesDeposit.data.balance, 700);
+        assert.strictEqual(stewiesDeposit.data.accountFrom, 1234);
+        assert.strictEqual(stewiesDeposit.data.transactionAmount, 600);
       } catch (error) {
+        console.error(error);
         assert.fail('Should not throw');
       }
     });
 
     it('Case 2: Glenn Quagmire throws a party', async () => {
       try {
-        await axios.post(getUrl('transactions'), {
-        });
+        const glennsParty: AxiosResponse[] = [];
+        
+        glennsParty.push(await axios.post(getUrl('transactions'), {
+          data: JSON.stringify({
+            // token: whatever we use for auth
+            accountFrom: 2001,
+            amount: -5000,
+            currency: 'MXN'
+          })
+        }));
+
+        glennsParty.push(await axios.post(getUrl('transactions'), {
+          data: JSON.stringify({
+            // token: whatever we use for auth
+            accountFrom: 2001,
+            amount: -12500,
+            currency: 'USD'
+          })
+        }));
+
+        glennsParty.push(await axios.post(getUrl('transactions'), {
+          data: JSON.stringify({
+            // token: whatever we use for auth
+            accountFrom: 2001,
+            amount: 300,
+            currency: 'CAD'
+          })
+        }));
+
+        const finalBalance = glennsParty[glennsParty.length - 1].data.balance;
+
+        assert.strictEqual(finalBalance, 700);
+        assert.strictEqual(glennsParty[0].data.transactionAmount, -5000 / 10);
+        assert.strictEqual(glennsParty[1].data.transactionAmount, -12500 * 2);
+        assert.strictEqual(glennsParty[2].data.transactionAmount, 3000);
       } catch (error) {
+        console.error(error);
         assert.fail('Should not throw');
       }
     });
 
-    it('Case 3: Joe Swanson takes a bribe from Joe Shark', async () => {
-      try {
-        await axios.post(getUrl('transactions'), {
-        });
-      } catch (error) {
-        assert.fail('Should not throw');
-      }
-    });
+    // it('Case 3: Joe Swanson takes a bribe from Joe Shark', async () => {
+    //   try {
+    //     await axios.post(getUrl('transactions'), {
+    //     });
+    //   } catch (error) {
+    //     assert.fail('Should not throw');
+    //   }
+    // });
 
-    it('Case 4: Lois gives Peter his allowance', async () => {
-      try {
-        await axios.post(getUrl('transactions'), {
-        });
-      } catch (error) {
-        assert.fail('Should not throw');
-      }
-    });
+    // it('Case 4: Lois gives Peter his allowance', async () => {
+    //   try {
+    //     await axios.post(getUrl('transactions'), {
+    //     });
+    //   } catch (error) {
+    //     assert.fail('Should not throw');
+    //   }
+    // });
 
-    it('Case 5: Joe Shark tries to get his money back from Joe Swanson', async () => {
-      try {
-        await axios.post(getUrl('transactions'), {
-        });
-      } catch (error) {
-        assert.fail('Should not throw');
-      }
-    });
+    // it('Case 5: Joe Shark tries to get his money back from Joe Swanson', async () => {
+    //   try {
+    //     await axios.post(getUrl('transactions'), {
+    //     });
+    //   } catch (error) {
+    //     assert.fail('Should not throw');
+    //   }
+    // });
 
-    it('Case 6: Peter and Lois sell Meg to Joe Shark', async () => {
-      try {
-        await axios.post(getUrl('transactions'), {
-        });
-      } catch (error) {
-        assert.fail('Should not throw');
-      }
-    });
-
-
-    it('Case ', async () => {
-      try {
-        await axios.post(getUrl('transactions'), {
-          headers: {
-            'Accept': 'text/html'
-          }
-        });
-      } catch (error) {
-        assert.fail('Should not throw');
-      }
-    });
+    // it('Case 6: Peter and Lois sell Meg to Joe Shark', async () => {
+    //   try {
+    //     await axios.post(getUrl('transactions'), {
+    //     });
+    //   } catch (error) {
+    //     assert.fail('Should not throw');
+    //   }
+    // });
 
     it('shows a 404 HTML page', async () => {
       try {
