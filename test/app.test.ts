70d1ffc0d2aaca2a6e4c324f7c5aa04925207458
Jacob Robinson <jrobinsondev@gmail.com>
2020-08-24 23:59:29 -0400

    outline end to end test cases

--- test/app.test.ts
+++ test/app.test.ts
@@ -31,7 +31,82 @@ describe('Feathers application tests', () => {
     assert.ok(data.indexOf('<html lang="en">') !== -1);
   });
 
-  describe('404', function() {
+  describe('End to End Testing', function() {
+    // These test cases correspond to the five cases provided 
+    // in the coding assignment as well as a sixth case to 
+    // cover the situation where two people share a joint 
+    // account
+    it('Case 1: Stewie Griffin makes a deposit', async () => {
+      try {
+        const stewiesDeposit = await axios.post(getUrl('transactions'), {
+          headers: {
+            'Accept': 'text/html'
+          }
+        });
+        // assert.equal(stewiesDeposit.finalBalance, 400)
+      } catch (error) {
+        assert.fail('Should not throw');
+      }
+    });
+
+    it('Case 2: Glenn Quagmire throws a party', async () => {
+      try {
+        await axios.post(getUrl('transactions'), {
+        });
+      } catch (error) {
+        assert.fail('Should not throw');
+      }
+    });
+
+    it('Case 3: Joe Swanson takes a bribe from Joe Shark', async () => {
+      try {
+        await axios.post(getUrl('transactions'), {
+        });
+      } catch (error) {
+        assert.fail('Should not throw');
+      }
+    });
+
+    it('Case 4: Lois gives Peter his allowance', async () => {
+      try {
+        await axios.post(getUrl('transactions'), {
+        });
+      } catch (error) {
+        assert.fail('Should not throw');
+      }
+    });
+
+    it('Case 5: Joe Shark tries to get his money back from Joe Swanson', async () => {
+      try {
+        await axios.post(getUrl('transactions'), {
+        });
+      } catch (error) {
+        assert.fail('Should not throw');
+      }
+    });
+
+    it('Case 6: Peter and Lois sell Meg to Joe Shark', async () => {
+      try {
+        await axios.post(getUrl('transactions'), {
+        });
+      } catch (error) {
+        assert.fail('Should not throw');
+      }
+    });
+
+
+    it('Case ', async () => {
+      try {
+        await axios.post(getUrl('transactions'), {
+          headers: {
+            'Accept': 'text/html'
+          }
+        });
+      } catch (error) {
+        assert.fail('Should not throw');
+      }
+    });
+
     it('shows a 404 HTML page', async () => {
       try {
         await axios.get(getUrl('path/to/nowhere'), {
@@ -45,20 +120,6 @@ describe('Feathers application tests', () => {
 
         assert.equal(response.status, 404);
         assert.ok(response.data.indexOf('<html>') !== -1);
-      }
-    });
-
-    it('shows a 404 JSON error without stack trace', async () => {
-      try {
-        await axios.get(getUrl('path/to/nowhere'));
-        assert.fail('should never get here');
-      } catch (error) {
-        const { response } = error;
-
-        assert.equal(response.status, 404);
-        assert.equal(response.data.code, 404);
-        assert.equal(response.data.message, 'Page not found');
-        assert.equal(response.data.name, 'NotFound');
       }
     });
   });
