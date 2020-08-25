5ea0b86733337c8bb95337f4116f67aa668ae4b6
Jacob Robinson <jrobinsondev@gmail.com>
2020-08-24 22:51:39 -0400

    generate models. they're known as services in feathers. if we were using persistence, these services would correspond to DB tables

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,11 @@
 import { Application } from '../declarations';
+import users from './users/users.service';
+import accounts from './accounts/accounts.service';
+import transactions from './transactions/transactions.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
+  app.configure(users);
+  app.configure(accounts);
+  app.configure(transactions);
 }
