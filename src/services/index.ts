1411baf9912a94120a3fc5801cf618b17cfc499b
Jacob Robinson <jrobinsondev@gmail.com>
2020-08-25 22:56:27 -0400

    start moving services to sqlite. this will help the framework make my life easier

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,11 +1,6 @@
 import { Application } from '../declarations';
-import users from './users/users.service';
-import accounts from './accounts/accounts.service';
-import transactions from './transactions/transactions.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
-  app.configure(users);
-  app.configure(accounts);
-  app.configure(transactions);
+  return undefined; // make eslint happy
 }
