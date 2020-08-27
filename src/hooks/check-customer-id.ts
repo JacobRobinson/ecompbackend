bfe421dd2f06c5d5e7d4f44c0137ec31e49b2510
Jacob Robinson <jrobinsondev@gmail.com>
2020-08-27 12:25:49 -0400

    ensure proper functioning of the check-customer hook. normally this would probably be handled by an auth service. which I should have generated with this app but would have complicated testing (tracking tokens and all this is a pain)

--- src/hooks/check-customer-id.ts
+++ src/hooks/check-customer-id.ts
@@ -1,6 +1,6 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
-import { Hook, HookContext, Service, Params } from '@feathersjs/feathers';
+import { Hook, HookContext, Paginated } from '@feathersjs/feathers';
 import { Accounts } from '../services/accounts/accounts.class';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -8,16 +8,17 @@ export default (options = {}): Hook => {
   return async (context: HookContext): Promise<HookContext> => {
     const accountsService: Accounts = context.app.services.accounts;
     const requestCustomerID: number = context.data.customerID;
-    const findParams: Params = {
-      query: {
-        primary: requestCustomerID
-      }
-    };
-    const accountCheck = await accountsService.find(findParams);
-    // console.log(accountCheck);
-    // if (accountCheck != requestCustomerID) {
-    //   throw new Error('Transaction denied: Wrong customer ID');
-    // }
+    const requestAccountNumber: string = context.data.accountFrom;
+    
+    const accounts: Paginated<any> = <Paginated<any>> await accountsService.find({query: {primary: requestCustomerID}});
+    const secondaryAccounts: Paginated<any> = <Paginated<any>> await accountsService.find({query: {secondary: requestCustomerID}});
+    
+    const accountNumbers: string[] = accounts.data.map((account: any) => account.accountNumber);
+    const secondaryAccountNumbers: string[] = secondaryAccounts.data.map((account: any) => account.accountNumber);
+
+    if (!accountNumbers.includes(requestAccountNumber) && !secondaryAccountNumbers.includes(requestAccountNumber)) {
+      throw new Error('Transaction denied: wrong customer');
+    }
 
     delete context.data.customerID;
 
